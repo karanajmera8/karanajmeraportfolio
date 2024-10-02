a105e27d67e8a9de5779d6a7f980d7917af75497
karanajmera8 <karanajmera8@gmail.com>
2024-10-02 13:24:23 -0700

    email validation

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,62 +1,101 @@
-import React, { useState, useContext, useRef } from 'react'
+import React, {useState, useContext, useRef} from 'react';
 import { SignupContext } from '../Contexts/SignupContext';
 import { Button } from './Button';
 import emailjs from '@emailjs/browser';
 
-function SignUp() {
-    const [fullName,setFullName] = useState('');
-    const [email,setEmail] = useState('');
-    const [note,setNote] = useState('');
+function SignUp(){
+    const [formData,setFormData] = useState({
+        name:'',
+        email:'',
+        note:'',
+    });
+
     const {signUp,setSignUp} = useContext(SignupContext)
+
     const form = useRef();
 
     const cancelClick = ()=>{
-      setSignUp(!signUp);
-      setFullName('');
-      setEmail('');
-      setNote('');
+        setSignUp(!signUp);
+        setFormData({
+            name:'',
+            email:'',
+            note:'',
+        })
+        setErrors({});
     }
 
-    const sendEmail = (e)=>{
-      e.preventDefault();
+    const [errors,setErrors] = useState({});
 
-      emailjs.sendForm('service_jymlpk5', 'template_p7f9r4v', form.current, '-egdd3uP4dza2YoER')
-        .then((result)=>{
-          console.log(result.text);
-        },(error)=>{
-          console.log(error.text);
-        });
-      
-      setSignUp(!signUp);
-      setFullName('');
-      setEmail('');
-      setNote('');
+    const handleChange = (e) => {
+        setFormData({...formData,[e.target.name]:e.target.value});
+    };
 
-      alert("Thank you for your email! I will get back to you ASAP!")
+    const validateForm = () => {
+        let newErrors = {};
+
+        if (!formData.name){
+            newErrors.name = 'Name is required';
+        }
+
+        if(!formData.email){
+            newErrors.email = 'Email is required';
+        }   else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(formData.email)){
+            newErrors.email = 'Invalid email format';
+        }
+
+        setErrors(newErrors);
+
+        return Object.keys(newErrors).length ===0;
     }
-    
-  return (signUp)?(
-    <>
-      <div className='pop-up'>
-        <div className='signup-form-container'>
-            <form ref = {form} className='signup-form'>
-                <label className='label' htmlFor='fullName'>Full Name: </label>
-                <input className='input' value ={fullName} onChange={(e)=>{setFullName(e.target.value)}} name='fullName' id='fullName'></input>
-                <br></br>
-                <label className='label' htmlFor='email'>Email: </label>
-                <input className='input' value ={email} onChange={(e)=>{setEmail(e.target.value)}} name='email' id='email'></input>
-                <br></br>
-                <label className='label' htmlFor='note'>Message: </label>
-                <textarea className='text-area' value ={note} onChange={(e)=>{setNote(e.target.value)}} name='note' id='note'></textarea>
-            </form>
-            <div className='button-holder'>
-              <Button className='signup-button' buttonStyle='btn--outline' onClick={cancelClick}>Cancel</Button>
-              <Button className='signup-button' buttonStyle='btn--primary' onClick={sendEmail}>Submit</Button>
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(validateForm()){
+            emailjs.sendForm('service_jymlpk5', 'template_p7f9r4v', form.current, '-egdd3uP4dza2YoER')
+            .then((result)=>{
+              console.log(result.text);
+            },(error)=>{
+              console.log(error.text);
+            });
+            setSignUp(!signUp);
+            setFormData({
+                name:'',
+                email:'',
+                note:'',
+            });
+            setErrors({});
+            alert('Thank you for your message! I will get back to you ASAP!')
+        }
+    };
+
+    return(signUp)?(
+        <>
+            <div className='pop-up'>
+                <div className='signup-form-container'>
+                    <form className='signup-form' ref = {form} onSubmit={handleSubmit}>
+                        <label className='label' htmlFor='name'>Full Name: </label>
+                        <input type = 'text' id='name' name='name' className='input' value ={formData.name} onChange={handleChange} ></input>
+                        {errors.name && <span className="error">{errors.name}</span>}
+                        
+                        <label className='label' htmlFor='email'>Email: </label>
+                        <input type = 'text' id='email' name='email' className='input' value ={formData.email} onChange={handleChange} ></input>
+                        {errors.email && <span className="error">{errors.email}</span>}
+
+                        <label className='label' htmlFor='note'>Message: </label>
+                        <input type = 'text-area' id='note' name='note' className='text-area' value ={formData.note} onChange={handleChange} ></input>
+                        {errors.note && <span className="error">{errors.note}</span>}
+                        <br></br>
+
+                        <div className='button-holder'>
+                            <Button className='signup-button' buttonStyle='btn--outline' onClick={cancelClick}>Cancel</Button>
+                            <Button className='signup-button' buttonStyle='btn--primary'>Submit</Button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-      </div>
-    </>
-  ):""
+        </>
+    ):""
 }
 
-export default SignUp
+export default SignUp;
